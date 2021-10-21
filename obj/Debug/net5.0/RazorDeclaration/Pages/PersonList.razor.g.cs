06b6e7b69f443e3393cb5b4ff0b928ec7dcd0e40
Chenben Tong <305594@via.dk>
2021-10-21 03:44:55 +0200

    aaa

--- obj/Debug/net5.0/RazorDeclaration/Pages/PersonList.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/PersonList.razor.g.cs
@@ -13,77 +13,77 @@ namespace LoginExample.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 1 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 2 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 3 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 4 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 5 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 6 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 7 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 8 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using LoginExample;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\_Imports.razor"
+#line 9 "D:\JetBrainsRider\Assignment1Finally12\_Imports.razor"
 using LoginExample.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\Pages\PersonList.razor"
+#line 2 "D:\JetBrainsRider\Assignment1Finally12\Pages\PersonList.razor"
 using FileData;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\Pages\PersonList.razor"
+#line 3 "D:\JetBrainsRider\Assignment1Finally12\Pages\PersonList.razor"
 using global::Models;
 
 #line default
@@ -98,7 +98,7 @@ using global::Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 49 "C:\Users\45527\RiderProjects\Sep3Assignment1\Assignment1Finally\Pages\PersonList.razor"
+#line 49 "D:\JetBrainsRider\Assignment1Finally12\Pages\PersonList.razor"
        
     private IList<Adult> _adults = new List<Adult>();
     private FileContext _fileContext = new FileContext();
